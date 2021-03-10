b1c73445da9517aa40800ffa00909a43012c40a9
VietIS Tan <tan.hoangngoc@vietis.com.vn>
2021-03-10 14:33:51 +0700

    first commit

--- src/store/store.js
+++ src/store/store.js
@@ -1,16 +1,16 @@
-import {createStore, applyMiddleware} from 'redux'
-import thunk from 'redux-thunk'
-import rootReducer from './reducers'
-import {composeWithDevTools} from 'redux-devtools-extension' 
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import rootReducer from "./reducers";
+import { composeWithDevTools } from "redux-devtools-extension";
 
-const initialState = {}
+const initialState = {};
 
-const middleware = [thunk]
+const middleware = [thunk];
 
 const store = createStore(
   rootReducer,
-  initialState,
+  initialState
   // composeWithDevTools(applyMiddleware(...middleware))
-)
+);
 
-export default store
+export default store;
